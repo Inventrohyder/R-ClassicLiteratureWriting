0dd251a100de0115f22e859218fef19007936e4b
Inventrohyder <haitham.hyder@minerva.kgi.edu>
2019-11-11 01:00:25 +0900

    Find Dickens' Works
    str_detect() from stringr allows partial string matching

--- time.R
+++ time.R
@@ -1,7 +1,10 @@
 library(readr)
 library(dplyr)
+library(stringr)
 
 titles <- read_csv('data/titles.csv')
 stats <-read.csv('data/stats.csv')
 
 books <- full_join(titles, stats)
+
+dickens <- filter(books, str_detect(author, 'Dickens'))
