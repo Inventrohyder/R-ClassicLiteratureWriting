763b67db3895c60466cdb85ee08f17c6f626a3b0
Inventrohyder <haitham.hyder@minerva.kgi.edu>
2019-11-11 01:01:41 +0900

    Refine columns
    There are several statistics about each literary work.
    However, we only need a few of these.

--- time.R
+++ time.R
@@ -8,3 +8,5 @@ stats <-read.csv('data/stats.csv')
 books <- full_join(titles, stats)
 
 dickens <- filter(books, str_detect(author, 'Dickens'))
+dickens_stats <- dickens %>% select(id, words, sentences, to_be_verbs, contractions, pauses, cliches, similes
+)
